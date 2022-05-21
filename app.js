d9059b3f329e53e5d6cf88f89e8d42aec3dc40bb
J Bae <juyonbae@gmail.com>
2022-05-20 20:57:18 -0400

    html structured

---
+++ app.js
@@ -0,0 +1,24 @@
+//Selector
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+
+
+//Functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    
+}
